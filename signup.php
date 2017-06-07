7a1d3377352666ae2c55d1c468f619f81668911b
pjetrododani <pjedod12@htl-shkoder.com>
2017-06-07 13:08:43 +0200

    Add files via upload

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
 
 <head>
    
-  <title>Webshop </title>
+  <title>Sign up Webshop </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
@@ -35,7 +35,7 @@
         <div class="row">
 			
 			<div class="col-md-12 col-lg-6 col-lg-offset-3">
-				<form role="form" method="POST" action="signup.php">
+				<form role="form" method="POST" action="?register=1">
 					<div class="form-group">
 						<label for="vname">Firstname:</label>
 						<input type="text" class="form-control" id="vname" name="vname">
@@ -46,86 +46,85 @@
 					</div>
 					<div class="form-group">
 						<label for="gebtag">Birthdate:</label>
-						<input type="text" class="form-control" id="gebtag" name="birthdate" placeholder="DD.MM.YYYY">
+						<input type="DATE" class="form-control" id="gebtag" name="gebtag" >
 					</div>
 					
-					<label class="radio-inline"><input type="radio" name="geschlecht" value="m">male</label>
-					<label class="radio-inline"><input type="radio" name="geschlecht" value="f">female</label>
+
 					
 					<div class="form-group">
 						<label for="email">Email address:</label>
 						<input type="email" class="form-control" id="email" name="email">
 					</div>
 					<div class="form-group">
-						<label for="pwd">Password:</label>
+						<label for="password">Password:</label>
 						<input type="password" class="form-control" id="password" name="password">
+					</div>
+					<div class="form-group">
+						<label for="pwd">Password repeat:</label>
+						<input type="password" class="form-control" id="password" name="password2">
 					</div>
 						<button type="submit" class="btn btn-default" name="sub">Submit</button>
 				</form>
 			
 				<!-- Formular verarbeiten -->
 				<?php
-					if(isset($_POST['sub'])) {
-						
-						// DB verbinden
-						/*require('dbconnect.php');
-						
-						
-						
-						// Insert-Statement 1
-						$insert1 = "INSERT INTO user VALUES (?,?,?,?,?)";
-						
-						$sth1 = $dbh->prepare($insert1);
-						
-						$pwdencrypt=password_hash($_POST['password'], PASSWORD_DEFAULT);
-						
-						$sth1->bindParam(1, $_POST['vname']);
-						$sth1->bindParam(2, $_POST['fname']);
-						$sth1->bindParam(3, $_POST['email']);
-						$sth1->bindParam(4, $PassHash);
-						$sth1->bindParam(5, $_POST['geschlecht']);
-						
-						$sth1->exec();
-						echo "Sign up successfully";
-						
-						
-						//SELECT-Statement
-						$select = "SELECT idU  FROM user WHERE email = ? ";
-						
-						$sth2 = $dbh->prepare($select);
-						
-						$sth2->bindParam(1, $_POST['email']);
-						
-						$sth2->execute();
-						
-						$result = $sth2->fetch(PDO::FETCH_ASSOC);
-						$idU = $result('idU'); */
-						$db_host = "localhost";
-						$db_user = "root";
-						$db_passwort = "";
-						$db_name = "folkflow";
-						mysql_connect("$db_host","$db_user","$db_passwort") or die
-						("Keine Verbindung moeglich");
-						mysql_select_db("$db_name") or die ("Die Datenbank existiert nicht.");
-						$vorname = $_POST['vname'];
-						$nachname = $_POST['fname'];
-						$email = $_POST['email'];
-						$psw = $_POST['password'];
-						$gebtag = $_POST['gebtag'];
-						$geschl = $_POST['geschlecht'];
-						if(strlen($vorname)>2 && strlen($nachname)>2){
-						$sql = 'INSERT INTO user (vname, fname, email, password, gebtag, geschlecht) VALUES ("'.$vorname.'","'.$nachname.'", "'.$email.'", "'.$psw.'",
-						"'.$gebtag.'","'.$geschl.'")';
-						$eintragen = mysql_query($sql) or die("Fehler: ".mysql_error());
-							 echo 'Der Eintrag war erfolgreich';
-						} else {
-							 echo 'Ihre Angaben sind fehlerhaft.';
-						}
+				$showFormular = true; //Variable ob das Registrierungsformular anezeigt werden soll
+				require 'dbconnect.php'; 
+				if(isset($_GET['register'])) {
+				 $error = false;
+				 $vname = $_POST['vname'];
+				 $fname = $_POST['fname'];
+				 $email = $_POST['email'];
+				 $password = $_POST['password'];
+				 $password2 = $_POST['password2'];
+				 $gebtag = $_POST['gebtag'];
+				 //$geschlecht = $_POST['geschlecht'];
+				  
+				 if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				 echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+				 $error = true;
+				 } 
+				 if(strlen($password) == 0) {
+				 echo 'Bitte ein Passwort angeben<br>';
+				 $error = true;
+				 }
+				 if($password != $password2) {
+				 echo 'Die Passwörter müssen übereinstimmen<br>';
+				 $error = true;
+				 }
+				 
+				 //Überprüfe, dass die E-Mail-Adresse noch nicht registriert wurde
+				 if(!$error) { 
+				 $statement = $pdo -> prepare("SELECT * FROM user WHERE email = :email ");
+				 $result = $statement->execute(array('email' => $email));
+				 $user = $statement->fetch();
+				 
+				 if($user !== false) {
+				 echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+				 $error = true;
+				 } 
+				 }
+				
+				 				 //Keine Fehler, wir können den Nutzer registrieren
+				 if(!$error) { 
+				 $password_hash = password_hash($password, PASSWORD_DEFAULT);
+				 
+				 $statement = $pdo->prepare("INSERT INTO user (vname, fname, email, password, gebtag ) VALUES (:vname, :fname, :email, :password, :gebtag)");
+				 $result = $statement->execute(array('vname' => $vname,'fname' => $vname, 'email' => $email, 'password' => $passwort_hash, 'gebtag' => $gebtag));
+				 
+				 if($result) { 
+				 echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+				 $showFormular = false;
+				 } else {
+				 echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+				 }
+				 } 
+				
 						
 						
 						// DB schliessen
 						$dbh = null;
-					}
+					
 				?>
 			</div>
             
@@ -148,6 +147,7 @@
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
-
+<?php }
+ ?>
 </body>
 </html>
